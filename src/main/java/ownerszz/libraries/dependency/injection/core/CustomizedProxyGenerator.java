e5b40db3e02ef9650b923f79edb5a9f1e5f0d389
Ownerszz <tneownerszz@gmail.com>
2021-01-16 22:26:14 +0100

    Shipping DependencyInjection-2.0.jar with all dependencies included

--- src/main/java/ownerszz/libraries/dependency/injection/core/CustomizedProxyGenerator.java
+++ src/main/java/ownerszz/libraries/dependency/injection/core/CustomizedProxyGenerator.java
@@ -4,26 +4,23 @@ package ownerszz.libraries.dependency.injection.core;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.NoOp;*/
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import org.objenesis.ObjenesisHelper;
+
 public class CustomizedProxyGenerator {
     /**
      *
      * @param interfaze the interfaze
-     * @return the interfaze
+     * @return the an instance that implements the given interface
      */
-    public static Object createInterfaceInstance(Class interfaze){
-        //TODO: Maybe in the future we will need this
-        /*Enhancer enhancer = new Enhancer();
-        enhancer.setClassLoader(Thread.currentThread().getContextClassLoader());
-        enhancer.setCallbackType(NoOp.class);
-        enhancer.setSuperclass(interfaze);
-        enhancer.setInterfaces(new Class[]{interfaze});
-        enhancer.setCallback(NoOp.INSTANCE);
-        enhancer.setUseCache(false);
-        enhancer.setUseFactory(false);
-        enhancer.setInterceptDuringConstruction( false );
-        //Class<T> temp = (Class<T>) enhancer.createClass();
-        //T instance = (T) enhancer.create();
-        return  (T) enhancer.create();*/
-        return interfaze;
+    public static <T> T createInterfaceInstance(Class<T> interfaze){
+        Class<? extends T> implClass = new ByteBuddy()
+                                        .subclass(interfaze)
+                                        .make()
+                                        .load(interfaze.getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+                                        .getLoaded();
+
+        return ObjenesisHelper.newInstance(implClass);
     }
 }
